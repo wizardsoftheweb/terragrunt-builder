cbd8d7f6bbfdc0c6c5dd2785bc624f96c0ca0d3c
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-30 10:11:05 -0500

    Comment out currently unused vars

--- parser/parser.go
+++ parser/parser.go
@@ -23,29 +23,29 @@ const (
 	DiagIgnoreUnsupportedAttribute = "unsupported attribute"
 )
 
-var (
-	// importantBlocksSchema sets up the blocks we're interested in as we parse TF
-	importantBlocksSchema = &hcl.BodySchema{
-		Blocks: []hcl.BlockHeaderSchema{
-			{
-				Type:       "variable",
-				LabelNames: []string{"name"},
-			},
-			{
-				Type:       "output",
-				LabelNames: []string{"name"},
-			},
-		},
-	}
-	// variableBlockSchema grabs only the attributes we're interested in from the variable block
-	variableBlockSchema = &hcl.BodySchema{
-		Attributes: []hcl.AttributeSchema{
-			{
-				Name: "default",
-			},
-		},
-	}
-)
+//var (
+//	// importantBlocksSchema sets up the blocks we're interested in as we parse TF
+//	importantBlocksSchema = &hcl.BodySchema{
+//		Blocks: []hcl.BlockHeaderSchema{
+//			{
+//				Type:       "variable",
+//				LabelNames: []string{"name"},
+//			},
+//			{
+//				Type:       "output",
+//				LabelNames: []string{"name"},
+//			},
+//		},
+//	}
+//	// variableBlockSchema grabs only the attributes we're interested in from the variable block
+//	variableBlockSchema = &hcl.BodySchema{
+//		Attributes: []hcl.AttributeSchema{
+//			{
+//				Name: "default",
+//			},
+//		},
+//	}
+//)
 
 type Variable struct {
 	Name    string
